cdaffc403e8e73d22987a4b794942cef5ee1c82a
Leonardo Marques <leommarques7@gmail.com>
2014-11-06 23:58:56 -0200

    login funcionando

--- app/src/main/java/com/leonardo/findya/InicioAct.java
+++ app/src/main/java/com/leonardo/findya/InicioAct.java
@@ -1,7 +1,9 @@
 package com.leonardo.findya;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.facebook.Request;
@@ -26,39 +28,25 @@ public class InicioAct extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /*
-        // Add code to print out the key hash
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.leonardo.findya",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-
-        }*/
-
-
         Session.openActiveSession(this, true, new Session.StatusCallback() {
 
             // callback when session changes state
             @Override
             public void call(Session session, SessionState state, Exception exception) {
+                Log.i("fb", "call");
                 if (session.isOpened()) {
-
+                    Log.i("fb", "session.isOpened()");
                     // make request to the /me API
                     Request.newMeRequest(session, new Request.GraphUserCallback() {
 
                         // callback after Graph API response with user object
                         @Override
                         public void onCompleted(GraphUser user, Response response) {
+                            Log.i("fb", "onCompleted");
                             if (user != null) {
+                                Log.i("fb", "user != null");
                                 usuario = user;
+                                aoCriar();
                             }
                         }
                     }).executeAsync();
@@ -71,5 +59,11 @@ public class InicioAct extends Activity {
     public void aoCriar() {
         if (usuario != null)
             texto.setText(usuario.getFirstName());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
     }
 }
