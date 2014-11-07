3f4c45070472d8241df5771c6a312cbfa738d7f0
Leonardo Marques <leommarques7@gmail.com>
2014-11-06 22:16:42 -0200

    hello world

--- app/src/main/java/com/leonardo/findya/InicioAct.java
+++ app/src/main/java/com/leonardo/findya/InicioAct.java
@@ -5,11 +5,36 @@ import android.os.Bundle;
 
 import org.androidannotations.annotations.EActivity;
 
-@EActivity
+@EActivity(R.layout.login)
 public class InicioAct extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        LoginAct_.intent(this).start();
+        super.onCreate(savedInstanceState);
+
+        /*
+        Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+            // callback when session changes state
+            @Override
+            public void call(Session session, SessionState state, Exception exception) {
+                if (session.isOpened()) {
+
+                    // make request to the /me API
+                    Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                        // callback after Graph API response with user object
+                        @Override
+                        public void onCompleted(GraphUser user, Response response) {
+                            if (user != null) {
+                                TextView welcome = (TextView) findViewById(R.id.welcome);
+                                welcome.setText("Hello " + user.getName() + "!");
+                            }
+                        }
+                    }).executeAsync();
+                }
+            }
+        });
+        */
     }
 }
