60b1ae5c5484f5793fc056e9342077bb6512b662
Leonardo Marques <leommarques7@gmail.com>
2014-11-07 02:15:02 -0200

    login no facebook com login button

--- app/src/main/java/com/leonardo/findya/InicioAct.java
+++ app/src/main/java/com/leonardo/findya/InicioAct.java
@@ -1,69 +1,19 @@
 package com.leonardo.findya;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
-import com.facebook.Request;
-import com.facebook.Response;
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.model.GraphUser;
-
-import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
-import org.androidannotations.annotations.ViewById;
 
-@EActivity(R.layout.login)
+@EActivity
 public class InicioAct extends Activity {
-
-    @ViewById
-    TextView texto;
-
-    GraphUser usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Session.openActiveSession(this, true, new Session.StatusCallback() {
 
-            // callback when session changes state
-            @Override
-            public void call(Session session, SessionState state, Exception exception) {
-                Log.i("fb", "call");
-                if (session.isOpened()) {
-                    Log.i("fb", "session.isOpened()");
-                    // make request to the /me API
-                    Request.newMeRequest(session, new Request.GraphUserCallback() {
-
-                        // callback after Graph API response with user object
-                        @Override
-                        public void onCompleted(GraphUser user, Response response) {
-                            Log.i("fb", "onCompleted");
-                            if (user != null) {
-                                Log.i("fb", "user != null");
-                                usuario = user;
-                                aoCriar();
-                            }
-                        }
-                    }).executeAsync();
-                }
-            }
-        });
-    }
-
-    @AfterViews
-    public void aoCriar() {
-        if (usuario != null)
-            texto.setText(usuario.getFirstName());
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
+        LoginAct_.intent(this).start();
+        finish();
     }
 }
---
+++ app/src/main/java/com/leonardo/findya/LoginAct.java
@@ -0,0 +1,95 @@
+package com.leonardo.findya;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.login)
+public class LoginAct extends FragmentActivity {
+    @ViewById
+    public LoginButton loginButton;
+    private UiLifecycleHelper uiHelper;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        uiHelper = new UiLifecycleHelper(this, callback);
+        uiHelper.onCreate(savedInstanceState);
+    }
+
+    @AfterViews
+    public void aoCriar() {
+        loginButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                if (user != null) {
+                    Log.i("fb", user.getName());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+
+        Session session = Session.getActiveSession();
+        if (session != null &&
+                (session.isOpened() || session.isClosed()) ) {
+            onSessionStateChange(session, session.getState(), null);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            Log.i("fb", "Logged in...");
+        } else if (state.isClosed()) {
+            Log.i("fb", "Logged out...");
+        }
+    }
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
+
+}
