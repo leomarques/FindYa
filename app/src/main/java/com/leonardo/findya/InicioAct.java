88e8d1c8f87ef09d5a4b2d927dbc3d539ec853c6
Leonardo Marques <leommarques7@gmail.com>
2014-11-06 22:50:54 -0200

    facebook sdk

--- app/src/main/java/com/leonardo/findya/InicioAct.java
+++ app/src/main/java/com/leonardo/findya/InicioAct.java
@@ -2,17 +2,30 @@ package com.leonardo.findya;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.login)
 public class InicioAct extends Activity {
+
+    @ViewById
+    TextView texto;
+
+    GraphUser usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        /*
         Session.openActiveSession(this, true, new Session.StatusCallback() {
 
             // callback when session changes state
@@ -27,14 +40,18 @@ public class InicioAct extends Activity {
                         @Override
                         public void onCompleted(GraphUser user, Response response) {
                             if (user != null) {
-                                TextView welcome = (TextView) findViewById(R.id.welcome);
-                                welcome.setText("Hello " + user.getName() + "!");
+                                usuario = user;
                             }
                         }
                     }).executeAsync();
                 }
             }
         });
-        */
+    }
+
+    @AfterViews
+    public void aoCriar() {
+        if (usuario != null)
+            texto.setText(usuario.getFirstName());
     }
 }
